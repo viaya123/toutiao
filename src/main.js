5889a9e42027ef58e14dfbc7701079b998b5bec7
viaya <1980674844@qq.com>
2019-12-18 11:27:37 +0800

    准备资源

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './styles/index.less' // 引入初始化样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   router,
