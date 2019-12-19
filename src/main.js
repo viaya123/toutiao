1159c0553a0675ac0b43b089fa67e7f4d4311b3c
viaya <1980674844@qq.com>
2019-12-19 10:02:29 +0800

    配置安装axios

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './styles/index.less' // 引入初始化样式
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less' // 引入初始化样式
+import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 默认黑马头条的默认地址
+Vue.prototype.$axios = axios // axios赋值给全局属性
 Vue.config.productionTip = false
-
 Vue.use(ElementUI)
-
 new Vue({
   router,
   render: h => h(App)
