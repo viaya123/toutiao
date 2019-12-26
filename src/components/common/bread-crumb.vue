2cd49cca221125ece43bc8b7b837b88fdd25396a
viaya <1980674844@qq.com>
2019-12-26 19:11:07 +0800

    面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 面包屑 -->
+  <el-breadcrumb>
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+      <el-breadcrumb>
+          <slot name="title"></slot>
+      </el-breadcrumb>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
-
+import BreadCrumb from './common/bread-crumb.vue'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader) // 全局注册
+    Vue.component('bread-crumb', BreadCrumb)
   }
 }
