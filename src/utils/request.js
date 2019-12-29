d6c2889ce4a56d302b1f64a46839f005a4e4fe06
viaya <1980674844@qq.com>
2019-12-29 12:54:51 +0800

    评论列表-列表分页及请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import router from '../router'
 import { Message } from 'element-ui'
+import JSONBig from 'json-bigint'
 
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 默认黑马头条的默认地址
 axios.interceptors.request.use(function (config) {
@@ -13,22 +14,30 @@ axios.interceptors.request.use(function (config) {
   // 对请求失败做处理
 
 })
+
+axios.defaults.transformResponse = function (data) {
+  return JSONBig.parse(data) // JSONBig替换JSON.parse
+}
+
 // 响应拦截 响应数据 回来 到达then方法之前
 axios.interceptors.response.use(function (response) {
+  debugger
   // 对响应数据做处理 执行成功时进入
   return response.data ? response.data : {}
 }, function (error) {
   // 执行失败时执行
   let status = error.response.status // 获取失败的状态码
-  let message = '未知错误'
+  let message = ''
   switch (status) {
     case 400:
       message = '请求参数错误'
       break
     case 403:
+      window.localStorage.removeItem('user-token') // 强制删除token
       router.push('/login')
       break
     case 401:
+      window.localStorage.removeItem('user-token') // 强制删除token
       router.push('/login') // this.$router.push()
       break
     case 507:
@@ -41,8 +50,9 @@ axios.interceptors.response.use(function (response) {
     default:
       break
   }
-  Message({ type: 'warning', message: message })
+  Message({ type: 'warning', message })
   //   希望 在异常处理函数中将所有的错误都处理完毕 不再进入catch  终止错误
-  return new Promise(function () {}) // 终止当前的错误
+  // return new Promise(function () {}) // 终止当前的错误
+  return Promise.reject(error)
 })
 export default axios
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -15,10 +15,20 @@
         <!-- 文字组件 -->
         <template slot-scope="obj">
           <el-button type="text" size="small">修改</el-button>
-          <el-button type="text" size="small">{{obj.row.comment_status ?  '关闭评论': '打开评论'}}</el-button>
+          <!-- 注册事件 -->
+          <el-button type="text" size="small" @click="openOrClose(obj.row)">{{obj.row.comment_status ?  '关闭评论': '打开评论'}}</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <el-row type="flex" justify="center" align="middle" style="height:100px">
+      <el-pagination background layout="prev, pager, next"
+      :current-page="page.currentPage"
+      :page-size="page.pageSize"
+      :total="page.total"
+       @current-change="changePage"></el-pagination>
+    </el-row>
+
   </el-card>
 </template>
 
@@ -26,27 +36,66 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        // 专门放置分页数据
+        total: 0, // 数据总条数
+        pageSize: 10, // 默认每页条
+        currentPage: 1 // 当前页码默认第一页
+      }
     }
   },
   methods: {
+    // 页码改变事件
+    changePage (newPage) {
+      // newPage是当前页码
+      this.page.currentPage = newPage
+      this.getComment()
+    },
     // 请求评论列表
     getComment () {
       this.$axios({
         // query 查询参数，get参数
         url: '/articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results // 获取评论列表数据给本身的data
+        this.page.total = result.data.total_count // 获取文章总数
       })
     },
     // 定义一个布尔值转化
     formatterBool (cellValue) {
+      // cellValue当前单元格的值
       return cellValue ? '正常' : '关闭'
+    },
+    // 打开或关闭评论
+    openOrClose (row) {
+      // let mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(row.comment_status ? '关闭评论将清除所有评论，读者也不能再进行评论，是否进行此操作?' : '此操作将开启评论，是否进行此操作?').then(() => {
+        // 用户确定调用接口
+        this.$axios({
+          method: 'put',
+          url: '/comments/status',
+          params: {
+            article_id: row.id.toString()
+          },
+          // 请求体的放data中
+          data: {
+            allow_comment: !row.comment_status
+          }
+        }).then(result => {
+          this.$message({
+            type: 'success',
+            message: '操作成功'
+          })
+          this.getComment() // 重新请求列表
+        })
+      })
     }
   },
   // 钩子函数
   created () {
+    // 调用请求数据方法
     this.getComment()
   }
 }
@@ -57,5 +106,8 @@ export default {
   margin-top: 10px;
   margin-left: 10px;
   margin-right: 10px;
+  .page{
+    margin-top: 50px
+  }
 }
 </style>
